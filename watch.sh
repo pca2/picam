a0c83597c0db76eb2643628e34ac652d04b75abe
Carleton Atwater <atwaterc@gmail.com>
2014-11-23 21:41:43 -0500

    Add find & rm old files

--- watch.sh
+++ watch.sh
@@ -1,15 +1,17 @@
 email_list=''
+capture_dir='/home/pi/capture'
 daemon() {
     chsum1=""
 
     while [[ true ]]
     do
-        chsum2=`find capture/ -type f -exec md5sum {} \;`
+        chsum2=`find $capture_dir -type f -exec md5sum {} \;`
         if [[ $chsum1 != $chsum2 ]] ; then           
             echo "change detected" | logger -t watch
             echo "Motion decteced" | mail -s "Motion detected" $email_list
-            /bin/bash dropbox_uploader.sh -s -q upload $HOME/capture/. .
+            /bin/bash dropbox_uploader.sh -s -q upload $capture_dir/. .
             chsum1=$chsum2
+            find $capture_dir -type f -mmin 10 -exec rm {} \;
         fi
         sleep 2
     done
