536670eb9baca5f2b4aadf97f4f2231aaa75a96b
pca2 <atwaterc@gmail.com>
2014-11-26 16:28:32 -0500

    add master_watth.sh

--- env.sh
+++ env.sh
@@ -3,3 +3,4 @@ email_list=''
 capture_dir='/home/pi/capture'
 dropbox_uploader=$DIR/dropbox_uploader.sh
 ddate=`date '+%T %F'`
+pid_file_location='/tmp/watch.pid'
---
+++ master_watch.sh
@@ -0,0 +1,13 @@
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $DIR/env.sh
+$pid=`cat $pid_file_location`
+
+if ps -p $pid > /dev/null
+then
+  echo "pid found not doing anything"
+else
+  echo "pid not found starting script"
+  /bin/bash watch.sh &
+  echo $! > $pid_file_location
+fi
+
