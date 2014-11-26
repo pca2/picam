bef001584978c1e0af98da0f6955b73168e49b1e
pca2 <atwaterc@gmail.com>
2014-11-26 15:15:25 -0500

    add env file and also run script cleanup command as motion user

--- watch.sh
+++ watch.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
-email_list=''
-capture_dir='/home/pi/capture'
-ddate=`date +%T %F`
+#Add containing folder to path
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+#load creds & vars file
+source $DIR/env.sh
+
 daemon() {
     chsum1=""
 
@@ -11,9 +13,10 @@ daemon() {
         if [[ $chsum1 != $chsum2 ]] ; then           
             echo "change detected" | logger -t watch
             echo "Motion decteced" | mail -s "Motion detected at $ddate" $email_list
-            /bin/bash dropbox_uploader.sh -s -q upload $capture_dir/. .
+            /bin/bash $dropbox_uploader -s -q upload $capture_dir/. .
             chsum1=$chsum2
-            find $capture_dir -type f -mmin 10 -exec rm {} \;
+            #Note the line below expects that the user running this script as sudo access w/o pw
+            sudo -u motion find $capture_dir -type f -mmin 10 -exec rm {} \;
         fi
         sleep 2
     done
